bcd2a61d6a3f0a57710516ee2e3d3acc97b71b14
haiphan <hai.phan@zoho.com>
2026-02-08 06:12:31 +0200

    Add LeetCode 110

---
+++ src/LeetCode.Library/Algorithms/Lc110.cs
@@ -0,0 +1,17 @@
+namespace LeetCode.Library.Algorithms;
+using LeetCode.Library.DataStructures;
+
+public class Lc110Solution {
+    public bool good = true;
+    public int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int l = dfs(root.left);
+        int r = dfs(root.right);
+        if (Math.Abs(l - r) > 1) good = false;
+        return Math.Max(l, r) + 1;
+    }
+    public bool IsBalanced(TreeNode root) {
+        dfs(root);
+        return good;
+    }
+}
---
+++ tests/LeetCode.Tests/Lc110Tests.cs
@@ -0,0 +1,28 @@
+using LeetCode.Library.Algorithms;
+using LeetCode.Library.DataStructures;
+using Xunit;
+
+namespace LeetCode.Tests;
+
+public class Lc110Tests {
+    public static TheoryData<string, bool> Lc110Data => new()
+    {
+        // tree, expectedResult
+        { "3,9,#,#,20,15,#,#,7,#,#", true },
+        { "1,2,2,3,3,#,#,#,#,#,#", false },
+    };
+    
+    [Theory]
+    [MemberData(nameof(Lc110Data))]
+    public void Test_IsBalanced(string tree, bool expected) {
+        // Arrange
+        var solution = new Lc110Solution();
+        var codec = new TreeCodec();
+        var root = codec.Deserialize(tree)!;
+        // Act
+        var result = solution.IsBalanced(root);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
