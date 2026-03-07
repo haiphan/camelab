ed5093a1dc78483e034325ad79be583ea0509c40
haiphan <hai.phan@zoho.com>
2026-03-07 18:13:02 +0200

    lc1888 siplify int value extraction

--- src/LeetCode.Library/Algorithms/Lc1888.cs
+++ src/LeetCode.Library/Algorithms/Lc1888.cs
@@ -6,17 +6,17 @@ public class Lc1888Solution {
         int c1 = 0;
         for (int i = 0; i < n; i++) {
             int flip = i & 1;
-            c1 += 1 - ((s[i] - '0') ^ flip);
+            c1 += 1 - ((s[i] & 1) ^ flip);
         }
         int c0 = n - c1;
         int ans = Math.Min(c0, c1);
         if (n % 2 == 0) {
             return ans;
         }
-        int p1 = 1 - (s[0] - '0');
+        int p1 = 1 - (s[0] & 1);
         for (int i = 1; i < n; i++) {
             int flip = i & 1;
-            p1 += 1 - ((s[i] - '0') ^ flip);
+            p1 += 1 - ((s[i] & 1) ^ flip);
             int p0 = i + 1 - p1;
             int s0 = c0 - p0;
             int s1 = c1 - p1;
