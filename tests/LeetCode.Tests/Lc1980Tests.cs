28d9c4457261732c08fcaa174799ad36d3e0ffd2
haiphan <hai.phan@zoho.com>
2026-03-17 00:43:48 +0200

    lc1980 remove unused data

--- tests/LeetCode.Tests/Lc1980Tests.cs
+++ tests/LeetCode.Tests/Lc1980Tests.cs
@@ -4,16 +4,16 @@ using Xunit;
 namespace LeetCode.Tests;
 
 public class Lc1980Tests {
-    public static TheoryData<string[], string> Lc1980Data => new()
+    public static TheoryData<string[]> Lc1980Data => new()
     {
-        // nums, expectedResult
-        { ["00","01"], "11" },
-        { ["111","011","001"], "000" },
+        // nums
+        { ["00","01"] },
+        { ["111","011","001"] },
     };
     
     [Theory]
     [MemberData(nameof(Lc1980Data))]
-    public void Test_FindDifferentBinaryString(string[] nums, string expected) {
+    public void Test_FindDifferentBinaryString(string[] nums) {
         // Arrange
         var solution = new Lc1980Solution();
 
