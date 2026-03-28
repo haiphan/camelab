1355321e3701dc67a0e35ecfbb1f2b2be60a31af
haiphan <hai.phan@zoho.com>
2026-03-28 13:32:32 +0200

    lc2573 update

--- src/LeetCode.Library/Algorithms/Lc2573.cs
+++ src/LeetCode.Library/Algorithms/Lc2573.cs
@@ -4,33 +4,30 @@ public class Lc2573Solution {
     public string FindTheString(int[][] lcp) {
         int n = lcp.Length;
         char[] res = new char[n];
-        int id = -1;
-        for (int i = 0; i < n; i++)
-        {
-            if (lcp[i][i] != n - i) return "";
-            if (res[i] != '\0') continue;
-            if (id >= 25) return "";
-            char currentChar = (char)('a' + ++id);
-            for (int j = i; j < n; j++)
-            {
-                int commonPrefix = lcp[i][j];
-                if (commonPrefix != lcp[j][i] || commonPrefix > n - j) return "";
-                if (commonPrefix > 0)
-                {
-                    if (res[j] != '\0' && res[j] != currentChar) return "";
-                    res[j] = currentChar;
+        char cur = 'a';
+        for (int i = 0; i < n; i++) {
+            if (res[i] == '\0') {
+                if (cur > 'z') return "";
+                res[i] = cur;
+                for (int j = i + 1; j < n; j++) {
+                    if (lcp[i][j] > 0) {
+                        res[j] = res[i];
+                    }
                 }
+                cur++;
             }
         }
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                int x = lcp[i][j];
-                if (lcp[j][i] != x || x + i > n) return "";
-                int y = i < n - 1 ? lcp[i + 1][j + 1] : 0;
-                y = res[i] == res[j] ? y + 1 : 0;
-                if (x != y) return "";
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (res[i] != res[j]) {
+                    if (lcp[i][j] > 0) return "";
+                } else {
+                    if (i == n - 1 || j == n - 1) {
+                        if (lcp[i][j] != 1) return "";
+                    } else {
+                        if (lcp[i][j] != lcp[i + 1][j + 1] + 1) return "";
+                    }
+                }
             }
         }
         return new string(res);
