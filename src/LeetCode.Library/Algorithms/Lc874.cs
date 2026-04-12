b67ae1e241d8be94193cf45025b37453882fe6b9
haiphan <hai.phan@zoho.com>
2026-04-12 05:49:07 +0300

    lc874 use linear probing

--- src/LeetCode.Library/Algorithms/Lc874.cs
+++ src/LeetCode.Library/Algorithms/Lc874.cs
@@ -1,14 +1,65 @@
 namespace LeetCode.Library.Algorithms;
 
 public class Lc874Solution {
-    public readonly record struct Obs(int X, int Y);
+    private sealed class LinearProbingSet {
+        private readonly uint[] keys;
+        private readonly bool[] used;
+        private readonly int mask;
+
+        public LinearProbingSet(int expectedCount) {
+            int capacity = 1;
+            int target = Math.Max(4, expectedCount * 4);
+            while (capacity < target) {
+                capacity <<= 1;
+            }
+
+            keys = new uint[capacity];
+            used = new bool[capacity];
+            mask = capacity - 1;
+        }
+
+        public void Add(uint key) {
+            int idx = Hash(key);
+            while (used[idx]) {
+                if (keys[idx] == key) {
+                    return;
+                }
+                idx = (idx + 1) & mask;
+            }
+
+            used[idx] = true;
+            keys[idx] = key;
+        }
+
+        public bool Contains(uint key) {
+            int idx = Hash(key);
+            while (used[idx]) {
+                if (keys[idx] == key) {
+                    return true;
+                }
+                idx = (idx + 1) & mask;
+            }
+            return false;
+        }
+
+        private int Hash(uint key) {
+            return (int)((key * 2654435761u) & (uint)mask);
+        }
+    }
+
+    private static uint Encode(int x, int y) {
+        const int shift = 30000;
+        const int width = 60001;
+        return (uint)((x + shift) * width + (y + shift));
+    }
+
     public int RobotSim(int[] commands, int[][] obstacles) {
         int m = commands.Length, n = obstacles.Length;
         int[][] dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]];
         int di = 0, x = 0, y = 0, maxDistSq = 0;
-        HashSet<Obs> obsSet = new();
+        LinearProbingSet obsSet = new(n);
         for (int i = 0; i < n; i++) {
-            obsSet.Add(new Obs(obstacles[i][0], obstacles[i][1]));
+            obsSet.Add(Encode(obstacles[i][0], obstacles[i][1]));
         }
         for (int i = 0; i < m; i++) {
             int cmd = commands[i];
@@ -20,7 +71,7 @@ public class Lc874Solution {
                 int[] dir = dirs[di];
                 for (int step = 0; step < cmd; step++) {
                     int nextX = x + dir[0], nextY = y + dir[1];
-                    if (obsSet.Contains(new Obs(nextX, nextY))) {
+                    if (obsSet.Contains(Encode(nextX, nextY))) {
                         break;
                     }
                     x = nextX;
