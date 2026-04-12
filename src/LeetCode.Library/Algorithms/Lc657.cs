1bc299f184c69e833fb12eb5eb35ecae38226c23
haiphan <hai.phan@zoho.com>
2026-04-12 06:17:34 +0300

    lc657 use lookup table

--- src/LeetCode.Library/Algorithms/Lc657.cs
+++ src/LeetCode.Library/Algorithms/Lc657.cs
@@ -1,15 +1,29 @@
 namespace LeetCode.Library.Algorithms;
 
 public class Lc657Solution {
+    private static readonly sbyte[] Dx = CreateDx();
+    private static readonly sbyte[] Dy = CreateDy();
+
+    private static sbyte[] CreateDx() {
+        sbyte[] dx = new sbyte[128];
+        dx['L'] = -1;
+        dx['R'] = 1;
+        return dx;
+    }
+
+    private static sbyte[] CreateDy() {
+        sbyte[] dy = new sbyte[128];
+        dy['D'] = -1;
+        dy['U'] = 1;
+        return dy;
+    }
+
     public bool JudgeCircle(string moves) {
         int x = 0, y = 0;
         foreach (char move in moves) {
-            switch (move) {
-                case 'U': y++; break;
-                case 'D': y--; break;
-                case 'L': x--; break;
-                case 'R': x++; break;
-            }
+            int code = move;
+            x += Dx[code];
+            y += Dy[code];
         }
         return x == 0 && y == 0;
     }
