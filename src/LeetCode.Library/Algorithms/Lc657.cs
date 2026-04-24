5ec57bb22c1ba7d4c9e448809583f7aed5a4d0b4
haiphan <hai.phan@zoho.com>
2026-04-24 07:18:12 +0300

    lc657 use arithmetic

--- src/LeetCode.Library/Algorithms/Lc657.cs
+++ src/LeetCode.Library/Algorithms/Lc657.cs
@@ -1,29 +1,26 @@
 namespace LeetCode.Library.Algorithms;
 
 public class Lc657Solution {
-    private static readonly sbyte[] Dx = CreateDx();
-    private static readonly sbyte[] Dy = CreateDy();
-
-    private static sbyte[] CreateDx() {
-        sbyte[] dx = new sbyte[128];
-        dx['L'] = -1;
-        dx['R'] = 1;
-        return dx;
+    private static int GetDx(char move) {
+        int m = move;
+        int bit1 = (m >> 1) & 1;
+        int horizontalMask = ((m >> 3) & 1) | bit1;
+        return ((bit1 << 1) - 1) * horizontalMask;
     }
 
-    private static sbyte[] CreateDy() {
-        sbyte[] dy = new sbyte[128];
-        dy['D'] = -1;
-        dy['U'] = 1;
-        return dy;
+    private static int GetDy(char move) {
+        int m = move;
+        int bit1 = (m >> 1) & 1;
+        int horizontalMask = ((m >> 3) & 1) | bit1;
+        int verticalMask = 1 - horizontalMask;
+        return (((m & 1) << 1) - 1) * verticalMask;
     }
 
     public bool JudgeCircle(string moves) {
         int x = 0, y = 0;
         foreach (char move in moves) {
-            int code = move;
-            x += Dx[code];
-            y += Dy[code];
+            x += GetDx(move);
+            y += GetDy(move);
         }
         return x == 0 && y == 0;
     }
