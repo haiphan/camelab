9ecd9f0c5539466b5daa4d14a3f34e1de4f96c40
haiphan <hai.phan@zoho.com>
2026-05-23 16:49:59 +0300

    lc3043 small optimization

--- src/LeetCode.Library/Algorithms/Lc3043.cs
+++ src/LeetCode.Library/Algorithms/Lc3043.cs
@@ -9,23 +9,37 @@ public class Lc3043Solution {
         return (int)Math.Log10(num) + 1;
     }
     public int LongestCommonPrefix(int[] arr1, int[] arr2) {
-        HashSet<int> seen = new HashSet<int>();
-        foreach (int num in arr1) {
+        // Always use the smaller array to build the prefix set
+        int[] small, large;
+        if (arr1.Length <= arr2.Length) {
+            small = arr1;
+            large = arr2;
+        } else {
+            small = arr2;
+            large = arr1;
+        }
+
+        HashSet<int> prefixSet = new HashSet<int>();
+        int maxPossible = 0;
+        foreach (int num in small) {
+            maxPossible = Math.Max(maxPossible, countDigits(num));
             int x = num;
             while (x > 0) {
-                seen.Add(x);
+                prefixSet.Add(x);
                 x /= 10;
             }
         }
 
         int ans = 0;
-        foreach (int num in arr2) {
+        foreach (int num in large) {
             int x = num;
             int dc = countDigits(x);
-            // Only check prefixes longer than current ans
             while (x > 0 && dc > ans) {
-                if (seen.Contains(x)) {
+                if (prefixSet.Contains(x)) {
                     ans = dc;
+                    if (ans == maxPossible) {
+                        return ans;
+                    }
                     break;
                 }
                 x /= 10;
