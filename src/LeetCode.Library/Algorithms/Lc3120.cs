8f0733e8883dfe4a8ae7b0756c0945f240060a22
haiphan <hai.phan@zoho.com>
2026-05-26 04:10:58 +0300

    lc3120 use arithmetic

--- src/LeetCode.Library/Algorithms/Lc3120.cs
+++ src/LeetCode.Library/Algorithms/Lc3120.cs
@@ -6,14 +6,9 @@ public class Lc3120Solution {
         int count = 0;
         int[] freq = new int[26];
         for (int i = 0; i < n; i++) {
-            int code = word[i] - 'a';
-            if (code >= 0 && code < 26) {
-                freq[code] |= 1;
-            } else
-            {
-                code = word[i] - 'A';
-                freq[code] |= 2;
-            }
+            int code = (word[i] | 32) - 'a';
+            int bit = word[i] <= 'Z' ? 2 : 1;
+            freq[code] |= bit;
         }
         for (int i = 0; i < 26; i++) {
             count += freq[i] == 3 ? 1 : 0;
