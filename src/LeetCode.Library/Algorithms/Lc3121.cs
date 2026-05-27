5eb75bb2068867930d1a5a72a55ea9ce9efc1ee5
haiphan <hai.phan@zoho.com>
2026-05-28 02:45:32 +0300

    lc3121 prune

--- src/LeetCode.Library/Algorithms/Lc3121.cs
+++ src/LeetCode.Library/Algorithms/Lc3121.cs
@@ -4,6 +4,7 @@ namespace LeetCode.Library.Algorithms;
 
 public class Lc3121Solution {
     public int NumberOfSpecialChars(string word) {
+        const int allLettersMask = (1 << 27) - 2;
         int upperMask = 0;
         int lowerMask = 0;
 
@@ -11,6 +12,12 @@ public class Lc3121Solution {
             int code = c & 31;
             int ci = (c >> 5) & 1;
             int bit = 1 << code;
+
+            // If this letter is already dead (upper seen, lower cleared), it can no longer affect the result.
+            if ((upperMask & bit) != 0 && (lowerMask & bit) == 0) {
+                continue;
+            }
+
             int upperSeen = (upperMask >> code) & 1;
             int value = 1 ^ (ci & upperSeen);
 
@@ -20,6 +27,10 @@ public class Lc3121Solution {
             // Lowercase writes computed value; uppercase has no effect.
             int lowerWriteMask = bit * ci;
             lowerMask = (lowerMask & ~lowerWriteMask) | ((value << code) * ci);
+
+            if (upperMask == allLettersMask && lowerMask == 0) {
+                return 0;
+            }
         }
 
         int specialMask = upperMask & lowerMask;
