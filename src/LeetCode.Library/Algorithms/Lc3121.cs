0a88a1643036ff69a5289e9d1971befc2208dc7e
haiphan <hai.phan@zoho.com>
2026-05-28 02:38:52 +0300

    lc3121 bit mask

--- src/LeetCode.Library/Algorithms/Lc3121.cs
+++ src/LeetCode.Library/Algorithms/Lc3121.cs
@@ -1,31 +1,28 @@
+using System.Numerics;
+
 namespace LeetCode.Library.Algorithms;
 
 public class Lc3121Solution {
     public int NumberOfSpecialChars(string word) {
-        int n = word.Length;
-        int count = 0;
-        int[] lowPos = new int[26];
-        int[] upPos = new int[26];
-        for (int i = 0; i < 26; i++) {
-            lowPos[i] = -1;
-            upPos[i] = -1;
+        int upperMask = 0;
+        int lowerMask = 0;
+
+        foreach (char c in word) {
+            int code = c & 31;
+            int ci = (c >> 5) & 1;
+            int bit = 1 << code;
+            int upperSeen = (upperMask >> code) & 1;
+            int value = 1 ^ (ci & upperSeen);
+
+            // Uppercase (ci=0) sets upper bit; lowercase (ci=1) has no effect.
+            upperMask |= bit * (ci ^ 1);
+
+            // Lowercase writes computed value; uppercase has no effect.
+            int lowerWriteMask = bit * ci;
+            lowerMask = (lowerMask & ~lowerWriteMask) | ((value << code) * ci);
         }
-        for (int i = 0; i < n; i++) {
-            char c = word[i];
-            int code = (c | (char)32) - 'a';
-            if ((c & (char)32) == 0) {
-                if (upPos[code] == -1) {
-                    upPos[code] = i;
-                }
-            } else {
-                lowPos[code] = i;
-            }
-        }
-        for (int i = 0; i < 26; i++) {
-            if (lowPos[i] != -1 && upPos[i] != -1 && lowPos[i] < upPos[i]) {
-                count++;
-            }
-        }
-        return count;
+
+        int specialMask = upperMask & lowerMask;
+        return BitOperations.PopCount((uint)specialMask);
     }
 }
--- tests/LeetCode.Tests/Lc3121Tests.cs
+++ tests/LeetCode.Tests/Lc3121Tests.cs
@@ -1,4 +1,5 @@
 using LeetCode.Library.Algorithms;
+using System.Diagnostics;
 using Xunit;
 
 namespace LeetCode.Tests;
@@ -8,6 +9,8 @@ public class Lc3121Tests {
     {
         // word, expectedResult
         { "aaAbcBC", 3 },
+        { "AbBCab", 0 },
+        { "aAa", 0 },
     };
     
     [Theory]
@@ -21,5 +24,73 @@ public class Lc3121Tests {
 
         // Assert
         Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    [Trait("Category", "Benchmark")]
+    public void Benchmark_NumberOfSpecialChars_CompareImplementations() {
+        var random = new Random(3121);
+        var samples = new string[300];
+        for (int i = 0; i < samples.Length; i++) {
+            samples[i] = CreateRandomWord(random, 2000);
+        }
+
+        var solution = new Lc3121Solution();
+
+        // Warm up JIT for both implementations.
+        for (int i = 0; i < 2; i++) {
+            foreach (string word in samples) {
+                _ = solution.NumberOfSpecialChars(word);
+                _ = PreviousBranchlessArrayVersion(word);
+            }
+        }
+
+        var swCurrent = Stopwatch.StartNew();
+        int checksumCurrent = 0;
+        for (int round = 0; round < 6; round++) {
+            foreach (string word in samples) {
+                checksumCurrent += solution.NumberOfSpecialChars(word);
+            }
+        }
+        swCurrent.Stop();
+
+        var swPrevious = Stopwatch.StartNew();
+        int checksumPrevious = 0;
+        for (int round = 0; round < 6; round++) {
+            foreach (string word in samples) {
+                checksumPrevious += PreviousBranchlessArrayVersion(word);
+            }
+        }
+        swPrevious.Stop();
+
+        Assert.Equal(checksumPrevious, checksumCurrent);
+        Console.WriteLine($"Lc3121 benchmark (ms) current={swCurrent.ElapsedMilliseconds}, previous={swPrevious.ElapsedMilliseconds}");
+    }
+
+    private static string CreateRandomWord(Random random, int length) {
+        char[] chars = new char[length];
+        for (int i = 0; i < length; i++) {
+            int letter = random.Next(26);
+            bool isUpper = random.Next(2) == 0;
+            chars[i] = (char)((isUpper ? 'A' : 'a') + letter);
+        }
+        return new string(chars);
+    }
+
+    private static int PreviousBranchlessArrayVersion(string word) {
+        int[] masks = new int[2];
+
+        foreach (char c in word) {
+            int code = c & 31;
+            int ci = (c >> 5) & 1;
+            int bit = 1 << code;
+            int upperSeen = (masks[0] >> code) & 1;
+            int value = 1 ^ (ci & upperSeen);
+
+            masks[ci] = (masks[ci] & ~bit) | (value << code);
+        }
+
+        int specialMask = masks[0] & masks[1];
+        return System.Numerics.BitOperations.PopCount((uint)specialMask);
     }
 }
