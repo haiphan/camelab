802cf5d490a6182302e2bed35c4327f589af1dc3
haiphan <hai.phan@zoho.com>
2026-05-28 23:11:24 +0300

    lc3093 use array based trie

--- src/LeetCode.Library/Algorithms/Lc3093.cs
+++ src/LeetCode.Library/Algorithms/Lc3093.cs
@@ -1,59 +1,76 @@
 namespace LeetCode.Library.Algorithms;
 
 
-public class TrieNode {
-    public int[] Children { get; } = new int[26];
-    public int BestIndex { get; set; } = 1000000000;
-    public int BestLength { get; set; } = 1000000000;
-    public TrieNode()
-    {
-        Children.AsSpan().Fill(-1);
-    }
-}
 public class Lc3093Solution {
     public int[] StringIndices(string[] wordsContainer, string[] wordsQuery) {
-        List<TrieNode> trie = new() { new TrieNode() };
+        const int AlphabetSize = 26;
+        const int NotFound = -1;
+
+        int totalChars = 0;
+        foreach (string word in wordsContainer)
+        {
+            totalChars += word.Length;
+        }
+
+        int capacity = totalChars + 1;
+        int[] children = new int[capacity * AlphabetSize];
+        Array.Fill(children, NotFound);
+
+        int[] bestIndex = new int[capacity];
+        int[] bestLength = new int[capacity];
+        Array.Fill(bestIndex, int.MaxValue);
+        Array.Fill(bestLength, int.MaxValue);
+
+        int nextNode = 1;
         int containerSize = wordsContainer.Length;
         for (int i = 0; i < containerSize; i++)
         {
-            int len = wordsContainer[i].Length;
+            string word = wordsContainer[i];
+            int len = word.Length;
             int curr = 0;
-            
-            if (len < trie[curr].BestLength || (len == trie[curr].BestLength && i < trie[curr].BestIndex)) {
-                trie[curr].BestLength = len;
-                trie[curr].BestIndex = i;
+
+            if (len < bestLength[curr] || (len == bestLength[curr] && i < bestIndex[curr])) {
+                bestLength[curr] = len;
+                bestIndex[curr] = i;
             }
+
             for (int j = len - 1; j >= 0; j--) {
-                int charIdx = wordsContainer[i][j] - 'a';
-                
-                if (trie[curr].Children[charIdx] == -1) {
-                    trie[curr].Children[charIdx] = trie.Count;
-                    trie.Add(new TrieNode());
+                int charIdx = word[j] - 'a';
+                int childSlot = curr * AlphabetSize + charIdx;
+
+                if (children[childSlot] == NotFound) {
+                    children[childSlot] = nextNode;
+                    nextNode++;
                 }
-                
-                curr = trie[curr].Children[charIdx];
-                
-                if (len < trie[curr].BestLength || (len == trie[curr].BestLength && i < trie[curr].BestIndex)) {
-                    trie[curr].BestLength = len;
-                    trie[curr].BestIndex = i;
+
+                curr = children[childSlot];
+
+                if (len < bestLength[curr] || (len == bestLength[curr] && i < bestIndex[curr])) {
+                    bestLength[curr] = len;
+                    bestIndex[curr] = i;
                 }
             }
         }
+
         int[] ans = new int[wordsQuery.Length];
         for (int i = 0; i < wordsQuery.Length; i++)
         {
             int curr = 0;
             string query = wordsQuery[i];
             int len = query.Length;
+
             for (int j = len - 1; j >= 0; j--) {
                 int charIdx = query[j] - 'a';
-                if (trie[curr].Children[charIdx] == -1) {
+                int next = children[curr * AlphabetSize + charIdx];
+                if (next == NotFound) {
                     break;
                 }
-                curr = trie[curr].Children[charIdx];
+                curr = next;
             }
-            ans[i] = trie[curr].BestIndex;
+
+            ans[i] = bestIndex[curr];
         }
+
         return ans;
     }
 }
