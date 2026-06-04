a011de9f7aa534aec815a32cba3c0dc984937b0e
haiphan <hai.phan@zoho.com>
2026-06-04 22:11:17 +0300

    lc3043 use linear probing

--- src/LeetCode.Library/Algorithms/Lc3043.cs
+++ src/LeetCode.Library/Algorithms/Lc3043.cs
@@ -1,31 +1,57 @@
 namespace LeetCode.Library.Algorithms;
 
 public class Lc3043Solution {
-    private int countDigits(int num) {
-        // use log10 to count the number of digits in num
-        if (num == 0) {
-            return 1;
-        }
-        return (int)Math.Log10(num) + 1;
-    }
-    public int LongestCommonPrefix(int[] arr1, int[] arr2) {
-        // Always use the smaller array to build the prefix set
-        int[] small, large;
-        if (arr1.Length <= arr2.Length) {
-            small = arr1;
-            large = arr2;
-        } else {
-            small = arr2;
-            large = arr1;
-        }
+    // Linear-probing hash set; 0 is the empty sentinel (all values >= 1)
+    private int[] _table = [];
+    private int _mask;
 
-        HashSet<int> prefixSet = new HashSet<int>();
+    // Next power of two >= n
+    private static int NextPow2(int n) {
+        if (n <= 1) return 1;
+        return 1 << (32 - System.Numerics.BitOperations.LeadingZeroCount((uint)(n - 1)));
+    }
+
+    private static int Hash(int x) => (int)((uint)(x * 0x9e3779b9) >> 12); // Fibonacci hash → upper bits
+
+    private void TableAdd(int x) {
+        int h = Hash(x) & _mask;
+        while (_table[h] != 0) {
+            if (_table[h] == x) return;
+            h = (h + 1) & _mask;
+        }
+        _table[h] = x;
+    }
+
+    private bool TableContains(int x) {
+        int h = Hash(x) & _mask;
+        while (_table[h] != 0) {
+            if (_table[h] == x) return true;
+            h = (h + 1) & _mask;
+        }
+        return false;
+    }
+
+    private static int CountDigits(int num) =>
+        num < 10 ? 1 : num < 100 ? 2 : num < 1000 ? 3 : num < 10000 ? 4 :
+        num < 100000 ? 5 : num < 1000000 ? 6 : num < 10000000 ? 7 :
+        num < 100000000 ? 8 : 9;
+
+    public int LongestCommonPrefix(int[] arr1, int[] arr2) {
+        // Always use the smaller array to build the prefix table
+        int[] small = arr1.Length <= arr2.Length ? arr1 : arr2;
+        int[] large = arr1.Length <= arr2.Length ? arr2 : arr1;
+
+        // Size table to ~40% load: max prefixes = small.Length * 8 digits
+        int tableSize = NextPow2(small.Length * 20);
+        _mask = tableSize - 1;
+        _table = new int[tableSize];
+
         int maxPossible = 0;
         foreach (int num in small) {
-            maxPossible = Math.Max(maxPossible, countDigits(num));
+            maxPossible = Math.Max(maxPossible, CountDigits(num));
             int x = num;
             while (x > 0) {
-                prefixSet.Add(x);
+                TableAdd(x);
                 x /= 10;
             }
         }
@@ -33,13 +59,11 @@ public class Lc3043Solution {
         int ans = 0;
         foreach (int num in large) {
             int x = num;
-            int dc = countDigits(x);
+            int dc = CountDigits(x);
             while (x > 0 && dc > ans) {
-                if (prefixSet.Contains(x)) {
+                if (TableContains(x)) {
                     ans = dc;
-                    if (ans == maxPossible) {
-                        return ans;
-                    }
+                    if (ans == maxPossible) return ans;
                     break;
                 }
                 x /= 10;
