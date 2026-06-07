49c1fb21f193eabcd9e2c8342fdda5bb4f43923e
haiphan <hai.phan@zoho.com>
2026-06-07 22:32:32 +0300

    lc665 use count buckets

--- src/LeetCode.Library/Algorithms/Lc1665.cs
+++ src/LeetCode.Library/Algorithms/Lc1665.cs
@@ -3,14 +3,41 @@ namespace LeetCode.Library.Algorithms;
 public class Lc1665Solution {
     public int MinimumEffort(int[][] tasks) {
         int n = tasks.Length;
-        // sort by min[i] - actual[i]
-        Array.Sort(tasks, (a, b) => (a[1] - a[0]).CompareTo(b[1] - b[0]));
+        const int MaxDelta = 9999;
+        int[] counts = new int[MaxDelta + 1];
+        int minDelta = MaxDelta;
+        int maxDelta = 0;
+
+        // Counting sort by delta = minimum - actual in ascending order.
+        foreach (int[] task in tasks) {
+            int delta = task[1] - task[0];
+            counts[delta]++;
+            if (delta < minDelta) {
+                minDelta = delta;
+            }
+            if (delta > maxDelta) {
+                maxDelta = delta;
+            }
+        }
+
+        for (int i = minDelta + 1; i <= maxDelta; i++) {
+            counts[i] += counts[i - 1];
+        }
+
+        int[][] sortedTasks = new int[n][];
+        for (int i = n - 1; i >= 0; i--) {
+            int[] task = tasks[i];
+            int delta = task[1] - task[0];
+            sortedTasks[--counts[delta]] = task;
+        }
+
         int ans = 0;
-        foreach (var task in tasks) {
+        foreach (int[] task in sortedTasks) {
             int actual = task[0];
             int minimum = task[1];
             ans = Math.Max(ans + actual, minimum);
         }
+
         return ans;
     }
 }
