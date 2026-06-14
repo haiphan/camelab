a9586d723828ef8209c4e8f2983edfd462fe4067
haiphan <hai.phan@zoho.com>
2026-06-14 06:17:26 +0300

    lc2130 improve typing

--- src/LeetCode.Library/Algorithms/Lc2130.cs
+++ src/LeetCode.Library/Algorithms/Lc2130.cs
@@ -5,10 +5,11 @@ namespace LeetCode.Library.Algorithms;
 public class Lc2130Solution {
     public int PairSum(ListNode head) {
         // Find midpoint while reversing first half in-place
-        ListNode prev = null, slow = head, fast = head;
+        ListNode? prev = null, fast = head;
+        ListNode? slow = head;
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
-            ListNode next = slow.next;
+            ListNode? next = slow!.next;
             slow.next = prev;
             prev = slow;
             slow = next;
@@ -18,7 +19,7 @@ public class Lc2130Solution {
         // Walk both halves to find max twin sum
         int maxSum = 0;
         while (slow != null) {
-            maxSum = Math.Max(maxSum, prev.val + slow.val);
+            maxSum = Math.Max(maxSum, prev!.val + slow.val);
             prev = prev.next;
             slow = slow.next;
         }
