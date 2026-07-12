ec00b7ecdf3b85a6fdf43948d9a99ec625cd74f9
haiphan <hai.phan@zoho.com>
2026-07-12 17:39:50 +0300

    lc1331 use index arr

--- src/LeetCode.Library/Algorithms/Lc1331.cs
+++ src/LeetCode.Library/Algorithms/Lc1331.cs
@@ -3,21 +3,27 @@ namespace LeetCode.Library.Algorithms;
 public class Lc1331Solution {
     public int[] ArrayRankTransform(int[] arr) {
         int n = arr.Length;
-        int[] sortedArr = new int[n];
-        Array.Copy(arr, sortedArr, n);
-        Array.Sort(sortedArr);
-
-        Dictionary<int, int> rankMap = new Dictionary<int, int>();
-        int rank = 1;
-        foreach (int num in sortedArr) {
-            if (!rankMap.ContainsKey(num)) {
-                rankMap[num] = rank++;
-            }
+        if (n == 0) {
+            return [];
         }
 
-        int[] result = new int[n];
+        int[] sortedIdx = new int[n];
         for (int i = 0; i < n; i++) {
-            result[i] = rankMap[arr[i]];
+            sortedIdx[i] = i;
+        }
+
+        Array.Sort(sortedIdx, (a, b) => arr[a].CompareTo(arr[b]));
+
+        int[] result = new int[n];
+        int rank = 1;
+        result[sortedIdx[0]] = rank;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[sortedIdx[i]] != arr[sortedIdx[i - 1]]) {
+                rank++;
+            }
+
+            result[sortedIdx[i]] = rank;
         }
 
         return result;
